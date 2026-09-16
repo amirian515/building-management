ceef61d48fd340d0acb9613920f570b99a6f5e68
Amirian515 <amir515komak@gmail.com>
2026-09-16 22:55:08 +0330

    project structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+     function Header (){
+        return(
+                      <header className="flex justify-between items-center bg-primary p-5 ">
+            <button className="p-3 bg-danger rounded-2xl text-white">خروج</button>
+            <h1 className="font-semibold text-white">پنل مالی واحد 702</h1>
+            </header>
+        )}
+
+
+export default Header
